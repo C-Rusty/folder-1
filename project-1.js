bf71c7295776c0619e142a15d6cdc035927cc7ee
Rostislav Chanoff <rostislav.chanoff@soft-fx.lv>
2024-07-23 13:43:12 +0300

    commit-5

--- project-1.js
+++ project-1.js
@@ -2,5 +2,6 @@ console.log(`some code`);
 console.log(`some new code`);
 console.log(`some new code once more`);
 console.log(`some new code once more time`);
+console.log(`some new code once more time again`);
 
 
